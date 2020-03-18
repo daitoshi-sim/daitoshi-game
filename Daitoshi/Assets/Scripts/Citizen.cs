5c2e567036db85cf3bd51aef5ac3b06bc11de3d5
Rishi Raj <rajminster@gmail.com>
2020-03-17 22:42:02 -0700

    Added base classes for simulation

---
+++ Daitoshi/Assets/Scripts/Citizen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Daitoshi;
+
+namespace Daitoshi
+{
+    public class Citizen : MonoBehaviour
+    {
+       public int workQuality;
+       public float cash;
+       public int frugality;
+       public int goodsNeeds;
+       public int servicesNeeds;
+       public int leisureNeeds;
+       public Job job;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Daitoshi/Assets/Scripts/Job.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Daitoshi;
+
+namespace Daitoshi
+{
+    public class Job : MonoBehaviour
+    {
+        public float salary;
+        public Building building;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
